09752d6375e6ee032409a753ea264d8b5760ed2f
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-13 01:46:51 +0530

    added css

--- app.js
+++ app.js
@@ -19,19 +19,28 @@ function calculateProfit(sellingPrice, costPrice){
 }
 
 function calculateProfitLoss(){
+    document.getElementById("main-section").className="";
+    if(purchasePrice.value && stockQuantity.value && currentPrice.value){
     let costPrice = purchasePrice.value * stockQuantity.value;
     let sellingPrice = currentPrice.value * stockQuantity.value;
-    if(sellingPrice>costPrice){
+    if(costPrice === sellingPrice){
+        resultDiv.innerText="No Profit No Loss ✌";
+    }
+    else if(sellingPrice>costPrice){
         let profitAmount = calculateProfit(sellingPrice,costPrice);
         resultDiv.innerText ="You made a Profit of ₹"+profitAmount[0]+" 🥳 & Your %age gains were: "+profitAmount[1]+"% 📈";
         document.getElementById("main-section").className="profit-theme";
     }else{
         let lossAmount = calculateLoss(sellingPrice, costPrice);
         resultDiv.innerText ="You made a Loss of ₹"+lossAmount[0]+" 😥 & Your %age Losses were: "+lossAmount[1]+"% 📉";
+        if(lossAmount[1]>50){
         document.getElementById("main-section").className="sad-theme";
+        }
     }
 
-    
+}else{
+    resultDiv.innerText="❗ Please Enter the required Information!";
+}
 }
 
 checkButton.addEventListener("click", calculateProfitLoss)
