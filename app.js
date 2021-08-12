8dd53f9e4d6a7d0426f0191e6bf1d3fb50be672d
Vidit Awasthi <viditawasthi2@gmail.com>
2021-08-13 02:50:05 +0530

    fix: indentation

--- app.js
+++ app.js
@@ -5,42 +5,41 @@ const checkButton = document.querySelector("#check-button");
 const resultDiv = document.querySelector("#result-div");
 
 
-function calculateLoss(sellingPrice, costPrice){
+function calculateLoss(sellingPrice, costPrice) {
     let loss = costPrice - sellingPrice;
-    let lossPercentage = (loss *100)/costPrice;
+    let lossPercentage = (loss * 100) / costPrice;
     return [loss, lossPercentage];
 }
 
-function calculateProfit(sellingPrice, costPrice){
+function calculateProfit(sellingPrice, costPrice) {
     let profit = sellingPrice - costPrice;
-    let profitPercentage = (profit *100)/costPrice;
+    let profitPercentage = (profit * 100) / costPrice;
     return [profit, profitPercentage];
 
 }
 
-function calculateProfitLoss(){
-    document.getElementById("main-section").className="";
-    if(purchasePrice.value && stockQuantity.value && currentPrice.value){
-    let costPrice = purchasePrice.value * stockQuantity.value;
-    let sellingPrice = currentPrice.value * stockQuantity.value;
-    if(costPrice === sellingPrice){
-        resultDiv.innerText="No Profit No Loss ✌";
-    }
-    else if(sellingPrice>costPrice){
-        let profitAmount = calculateProfit(sellingPrice,costPrice);
-        resultDiv.innerText ="You made a Profit of ₹"+profitAmount[0]+" 🥳 & Your %age gains were: "+profitAmount[1]+"% 📈";
-        document.getElementById("main-section").className="profit-theme";
-    }else{
-        let lossAmount = calculateLoss(sellingPrice, costPrice);
-        resultDiv.innerText ="You made a Loss of ₹"+lossAmount[0]+" 😥 & Your %age Losses were: "+lossAmount[1]+"% 📉";
-        if(lossAmount[1]>50){
-        document.getElementById("main-section").className="sad-theme";
+function calculateProfitLoss() {
+    document.getElementById("main-section").className = "";
+    if (purchasePrice.value && stockQuantity.value && currentPrice.value) {
+        let costPrice = purchasePrice.value * stockQuantity.value;
+        let sellingPrice = currentPrice.value * stockQuantity.value;
+        if (costPrice === sellingPrice) {
+            resultDiv.innerText = "No Profit No Loss ✌";
+        } else if (sellingPrice > costPrice) {
+            let profitAmount = calculateProfit(sellingPrice, costPrice);
+            resultDiv.innerText = "You made a Profit of ₹" + profitAmount[0] + " 🥳 & Your %age gains were: " + profitAmount[1] + "% 📈";
+            document.getElementById("main-section").className = "profit-theme";
+        } else {
+            let lossAmount = calculateLoss(sellingPrice, costPrice);
+            resultDiv.innerText = "You made a Loss of ₹" + lossAmount[0] + " 😥 & Your %age Losses were: " + lossAmount[1] + "% 📉";
+            if (lossAmount[1] > 50) {
+                document.getElementById("main-section").className = "sad-theme";
+            }
         }
-    }
 
-}else{
-    resultDiv.innerText="❗ Please Enter the required Information!";
-}
+    } else {
+        resultDiv.innerText = "❗ Please Enter the required Information!";
+    }
 }
 
 checkButton.addEventListener("click", calculateProfitLoss)
